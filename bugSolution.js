d0eac9ed046178bbf92618898016f0ed6e2454d8
BugHunter-X <askzenai@gmail.com>
2025-02-13 19:24:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+const port = 8080;
+
+function startServer() {
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
+
+function handleError(err) {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${port} is already in use. Retrying in 1 second...`);
+    setTimeout(startServer, 1000);
+  } else {
+    console.error(`An error occurred: ${err.message}`);
+  }
+}
+
+server.on('error', handleError);
+
+startServer();
